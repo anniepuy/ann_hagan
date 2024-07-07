aa0dfb6bcd167d8f0b54fa029bc54d35e1da0605
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2024-07-07 12:18:09 -0500

    updates

---
+++ src/Components/navbar.js
@@ -0,0 +1,18 @@
+
+function Header() {
+  return (
+    <div className="Header">
+        <h1 id="nav-title">Ann Hagan</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+      
+    </div>
+  );
+}
+
+export default Header;
